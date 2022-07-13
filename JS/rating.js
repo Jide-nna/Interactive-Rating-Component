f23f07cd310e40bc0c971f090103344e2036039c
Bakinson Babajide <93973753+Jide-nna@users.noreply.github.com>
2022-07-13 17:49:19 +0100

    Omo i no even sabi wetin i commit..

--- JS/rating.js
+++ JS/rating.js
@@ -1,2 +1,13 @@
+let btns = document.querySelectorAll('.btn-container button')
+let card2 = document.querySelector('card-2')
+let card = document.querySelector('card')
+let span = document.querySelector('span')
 
-document.getElementsByClassName("thanks")
+Array.from(btns).forEach(btn => {
+    btn.addEventListener('click' , e => {
+        const rating = e.target.textConent
+        span.textContent = rating
+        card2.classList.remove('hide')
+        card.classList.add('hide')
+    })
+})
