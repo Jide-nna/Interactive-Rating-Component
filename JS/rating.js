7f81ceeb31329d578c583213f5c8a4fa1c25b042
Bakinson Babajide <93973753+Jide-nna@users.noreply.github.com>
2022-08-03 15:42:59 +0100

    Life Tuff...

--- JS/rating.js
+++ JS/rating.js
@@ -1,13 +1,19 @@
-let btns = document.querySelectorAll('.btn-container button')
-let card2 = document.querySelector('card-2')
-let card = document.querySelector('card')
+let btns = document.getElementsByClassName('rate')
+let submit = document.getElementsByClassName('submit')
+let card2 = document.getElementsByClassName('card-2')
+let card = document.getElementsByClassName('.card')
 let span = document.querySelector('span')
 
 Array.from(btns).forEach(btn => {
     btn.addEventListener('click' , e => {
-        const rating = e.target.textConent
-        span.textContent = rating
-        card2.classList.remove('hide')
-        card.classList.add('hide')
+        const rating = e.target.textContent
+        span.textContent = rating;
+       // card2.style.display = "visible"
+      //  card.style.display ='none'
     })
 })
+
+// Array.from(submit).addEventListener('click', e => {
+    card2.style.display = "block"
+    card.style.display ='none'
+// }) 
