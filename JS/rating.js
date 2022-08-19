9f34abde19fd36847a392a0ead43e556b0c61442
Bakinson Babajide <93973753+Jide-nna@users.noreply.github.com>
2022-08-19 01:35:18 +0100

    commits

--- JS/rating.js
+++ JS/rating.js
@@ -1,19 +1,22 @@
-let btns = document.getElementsByClassName('rate')
-let submit = document.getElementsByClassName('submit')
-let card2 = document.getElementsByClassName('card-2')
-let card = document.getElementsByClassName('.card')
-let span = document.querySelector('span')
+let btns = document.getElementsByClassName("rate");
+let submitBtn = document.getElementsByClassName("submit");
+let card2 = document.querySelector("#contain");
+let card = document.querySelector("#container");
+let span = document.querySelector("span");
+let submit = document.querySelector(".submit-p");
+let para = document.querySelector(".thanks p");
+Array.from(btns).forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const rating = e.target.textContent;
+    span.textContent = rating;
+  });
+});
 
-Array.from(btns).forEach(btn => {
-    btn.addEventListener('click' , e => {
-        const rating = e.target.textContent
-        span.textContent = rating;
-       // card2.style.display = "visible"
-      //  card.style.display ='none'
-    })
-})
-
-// Array.from(submit).addEventListener('click', e => {
-    card2.style.display = "block"
-    card.style.display ='none'
-// }) 
+submit.addEventListener("click", (e) => {
+  if (span.textContent) {
+    card.style.display = "none";
+    card2.style.display = "block";
+  } else {
+    para.textContent = "Choose a rating";
+  }
+});
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Interactive Rating Component
  An interactive Rating Component.
-I created two different html pages, one for the rating and the other for the thank you.
-Then linked the two html to the javaScript file.
+I 
  
